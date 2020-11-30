59dbd4abb1a1fd1b5752a90ae9d614120351a9c4
StephanXu <xuzihanapple@live.com>
2020-12-01 03:45:42 +0800

    fix missing library.ts

--- backend/src/controller/library.ts
+++ backend/src/controller/library.ts
@@ -1,12 +1,19 @@
 import { Router, Request, Response } from 'express'
+import { Author } from '../entity/author';
+import { Book } from '../entity/books';
 import { Library } from '../entity/library'
-import books from './books';
 
 const router = Router();
 
-type AddLibraryRequest = {
+interface AddLibraryRequest {
     name: string,
     position: string
+};
+
+interface BookReq {
+    title: string;
+    author: string[];
+    library: string;
 };
 
 router.route('/')
@@ -32,6 +39,47 @@ router.route('/:library')
         await library?.remove();
     });
 
-router.use('/:library/book', books)
+router.route('/:library/book')
+    .get(async (request: Request, response: Response) => {
+        let libraryId = request.params.library
+        let library = await Library.findOne({
+            where: { id: libraryId },
+            relations: ['book']
+        })
+        if (!library) {
+            response.status(404).send({ msg: 'Library not found' })
+            return;
+        }
+        response.json(library?.book);
+    })
+    .post(async (request: Request, response: Response) => {
+        let bookReq: BookReq = request.body;
+        let libraryId = request.params.library
+        let library = await Library.findOne({ where: { id: libraryId } })
+
+        if (!library) {
+            response.status(400).send({ msg: 'Library not found' })
+            return;
+        }
+
+        let newBook = new Book;
+        newBook.title = bookReq.title;
+        newBook.library = library;
+        newBook.reader = null;
+        let authorList: Author[] = [];
+
+        bookReq.author.forEach(async item => {
+            let author = await Author.findOne({ where: { name: item }, relations: ['book'] })
+            if (!author) {
+                author = new Author;
+                author.name = item;
+            }
+            authorList.push(author);
+        });
+        newBook.author = authorList;
+
+        await newBook.save();
+        response.status(200).send();
+    });
 
 export default router;
